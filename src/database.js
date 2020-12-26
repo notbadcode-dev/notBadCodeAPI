b6461e060dbfa0495b094226990a8b7829d82993
NotBadCode <notbadcode.dev@gmail.com>
2020-12-26 23:54:27 +0100

    revert database config

--- src/database.js
+++ src/database.js
@@ -1,11 +1,17 @@
 import  mongoose from 'mongoose';
 import config from './config';
 
-import options from './database.options.json';
-
 (async () => {
     try {
-        const db = await mongoose.connect(config.mongodbURL, options);
+        const db = await mongoose.connect(config.mongodbURL, {
+            user: 'root',
+            pass: '6900.$mongodbpass',
+            authSource: 'admin',
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        });
         console.log('Connect to database:', db.connection.name);
     } catch (error) {
         console.error(error);
